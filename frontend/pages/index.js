e1bbf06a7d49e3a3e7e2699af33360cd0bd99e59
Janabil03 <janabil03@gmail.com>
2024-05-12 20:08:38 +0300

    frist commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is a sample Next.js app.</p>
+    </div>
+  );
+};
+
+export default HomePage;
